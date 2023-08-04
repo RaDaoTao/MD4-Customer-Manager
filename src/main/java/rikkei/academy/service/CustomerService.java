de906b199e24aa7e4307ba405688c5b0982c16ff
hunghx <baby10051999@gmail.com>
2023-08-04 15:06:30 +0700

    uppp

--- src/main/java/rikkei/academy/service/CustomerService.java
+++ src/main/java/rikkei/academy/service/CustomerService.java
@@ -6,36 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerService implements IGenericService<Customer,Integer>{
-    private List<Customer> customers;
 
     public CustomerService() {
-        customers = new ArrayList<>();
     }
 
     @Override
     public List<Customer> findAll() {
-        return customers;
+
     }
 
     @Override
     public void save(Customer customer) {
-        if(findById(customer.getId())==null){
-            // thêm mới
-            customers.add(customer);
-        }else {
-            // sửa
-            customers.set(customers.indexOf(findById(customer.getId())), customer);
-        }
+
     }
 
     @Override
     public Customer findById(Integer integer) {
-        for (Customer c:customers) {
-            if(c.getId()==integer){
-                return c;
-            }
-        }
-        return null;
+
+
     }
     public int getNewId(){
         int idMax = 0;
@@ -49,6 +37,6 @@ public class CustomerService implements IGenericService<Customer,Integer>{
     }
     @Override
     public void deleteById(Integer integer){
-        customers.remove(findById(integer));
+
     }
 }
